25177a55627e0eaf9481f7a0feb723f3d34ee2e1
ayaney85 <osma8251@stthomas.edu>
2020-12-14 22:26:29 -0600

    Refactor method isRegularSkunk

--- H6_Skunk/src/SkunkController.java
+++ H6_Skunk/src/SkunkController.java
@@ -80,7 +80,7 @@ public class SkunkController
 					wantsToRoll = false;
 					break;
 				}
-				else if (skunkDice.getDie1().getLastRoll() == 1 || skunkDice.getDie2().getLastRoll() == 1)
+				else if (isRegularSkunk())
 				{
 					ui.println("One Skunk! You lose the turn, zeroing out the turn score and paying 1 chip to the kitty");
 					kitty += 1;
@@ -166,7 +166,7 @@ public class SkunkController
 					wantsToRoll = false;
 
 				}
-				else if (skunkDice.getDie1().getLastRoll() == 1 || skunkDice.getDie2().getLastRoll() == 1)
+				else if (isRegularSkunk())
 				{
 					ui.println("One Skunk!  You lose the turn, zeroing out the turn score and paying 1 chip to the kitty");
 					kitty += 1;
@@ -235,6 +235,11 @@ public class SkunkController
 
 		ui.println("-----------------------");
 		return true;
+	}
+
+	private boolean isRegularSkunk()
+	{
+		return skunkDice.getDie1().getLastRoll() == 1 || skunkDice.getDie2().getLastRoll() == 1;
 	}
 
 	private boolean getRollChoice()
